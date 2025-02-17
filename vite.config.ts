a8c57eb1da082fc87be814ff65b3d984f26d00bc
konrad <konradkopko1@gmail.com>
2025-02-16 21:05:26 -0500

    quick update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/konradkop.github.io',
+  base: './konradkop.github.io',
   plugins: [react()],
 });
