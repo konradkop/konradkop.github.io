8b2e7a79353de479c7fce5936705d6b9ea016bac
konrad <konradkopko1@gmail.com>
2025-09-11 16:21:02 -0400

    increating friction

--- src/pages/Ball.tsx
+++ src/pages/Ball.tsx
@@ -65,7 +65,7 @@ const Ball: React.FC<BallProps> = ({ open, labelsObj, setCurrLabel }) => {
           labelObj.ballSize,
           {
             restitution: 0.8,
-            frictionAir: 0.001,
+            frictionAir: 1,
             render: {
               fillStyle: labelObj.color,
               sprite: {
